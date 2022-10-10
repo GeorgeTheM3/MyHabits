99c271607ad5ad0236547de7a6e73542c9e80793
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2022-10-10 21:30:04 +0300

    added habits collection view

---
+++ MyHabits/Views/HabitsView.swift
@@ -0,0 +1,44 @@
+//
+//  HabitsView.swift
+//  MyHabits
+//
+//  Created by Георгий Матченко on 10.10.2022.
+//
+
+import Foundation
+import UIKit
+
+class HabitsView: UIView {
+    private lazy var habitsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setConstraintsSubviews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addSubviews()
+        setConstraintsSubviews()
+    }
+    
+    private func addSubviews() {
+        addSubview(habitsCollectionView)
+    }
+    
+    private func setConstraintsSubviews() {
+        NSLayoutConstraint.activate([
+            habitsCollectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            habitsCollectionView.widthAnchor.constraint(equalTo: widthAnchor),
+            habitsCollectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
